da86dca6a4960155d9beb38f5047eb0bb4eb2bd1
Fadli1009 <argyfa3@gmail.com>
2024-07-12 20:21:07 +0700

    update v3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ import Skils from "./Skils";
 import Projects from "./Projects";
 import Contact from "./Contack";
 import Footer from "./Footer";
-import downloadCV from '../assets/files/cvfadli.pdf'
+import downloadCV from '../assets/files/cvfadlinew.pdf'
 const Home = () => {
     const ctaEmail = 'mailto:m.fadlikurniawan1009@gmail.com'
     const judulAbout = "About Me"
